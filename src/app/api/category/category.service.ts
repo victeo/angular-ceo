46d823eea25274b6437970d79bc4b34cefaead64
victeo <victor.atomo@gmail.com>
2023-11-08 16:59:34 -0300

    Criação e delete das categorias

--- src/app/api/category/category.service.ts
+++ src/app/api/category/category.service.ts
@@ -2,21 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+const API_URL = '/api/categoria/v1';
+
 interface Categoria {
     id: number;
     nome: string;
-  }
+}
 
 @Injectable({
     providedIn: 'root'
 })
 
+
 export class CategoryService {
 
     constructor(private readonly http: HttpClient) { }
     
-
     getCategorias(): Observable<Categoria[]> {
         return this.http.get<Categoria[]>('/api/categoria/v1');
+    }
+
+    postCategoria(categoria: Categoria): Observable<Categoria> {
+        return this.http.post<Categoria>('/api/categoria/v1', categoria);
+    }
+
+    deleteCategoria(id: number): Observable<any> {
+        const url = `${'http://20.88.41.42/api/categoria/v1/'}${id}`;
+        return this.http.delete<any>(url, { observe: 'response' });
     }
 }
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild, ElementRef } from '@angular/core';
 import { CategoryService } from 'src/app/api/category/category.service';
 
 interface Categoria {
@@ -13,8 +13,11 @@ interface Categoria {
 })
 export class CategoryComponent {
 
+    @ViewChild('nameCategory') nameCategory?: ElementRef;
+
+
     constructor(@Inject(CategoryService) private categoryService: CategoryService) { }
-    
+
     categorias: Categoria[] = [];
 
     ngOnInit(): void {
@@ -29,5 +32,38 @@ export class CategoryComponent {
                 console.error('Erro ao buscar categorias', error);
             }
         );
+    }
+
+
+    excluirCategoria(id: number): void {
+        this.categoryService.deleteCategoria(id).subscribe(
+            (response) => {
+                console.log('Resposta:', response);
+                console.info(`Categoria com ID ${id} excluída com sucesso.`);
+                window.location.reload()
+            },
+            (error) => {
+                console.error(`Erro ao excluir categoria: ${error}`);
+            }
+        );
+
+    }
+    criarCategoria(): void {
+        if (this.nameCategory) {
+            const nome = this.nameCategory.nativeElement.value;
+
+            if (nome.trim() !== '') {
+                const novaCategoria = { id: null as unknown as number, nome }; // Corrigido aqui
+                this.categoryService.postCategoria(novaCategoria).subscribe(
+                    (categoria) => {
+                        console.log('Nova categoria criada:', categoria);
+                        window.location.reload()
+                    },
+                    (error) => {
+                        console.error('Erro ao criar categoria:', error);
+                    }
+                );
+            }
+        }
     }
 }
