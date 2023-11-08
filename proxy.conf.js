9a700251d06b9bfd8e25de9bfa3094c329fab861
victeo <victor.atomo@gmail.com>
2023-11-08 14:32:26 -0300

    create a proxy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,19 +1,6 @@
-var winston = require("winston");
-
-function logProvider() {
-  return winston.createLogger({
-    level: "debug",
-    format: winston.format.combine(
-      winston.format.splat(),
-      winston.format.simple()
-    ),
-    transports: [new winston.transports.Console()],
-  });
-}
-
 const PROXY_CONF = [
     {
-      context: ["/api"],
+      context: ["/api/"],
       target: "http://20.88.41.42",
       secure: false,
       changeOrigin: true
